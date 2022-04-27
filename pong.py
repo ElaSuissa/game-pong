abc6ae1eb5dfa97e59d692676a7b2b78a66cf2cf
ElaSuissa <97167034+ElaSuissa@users.noreply.github.com>
2022-04-27 15:14:38 +0300

    lines and more...

--- pong.py
+++ pong.py
@@ -1,30 +1,66 @@
-from pickle import TRUE
+from cv2 import circle
+from numpy import square
 import pygame
-# screen size 
+import time
+from pickle import TRUE
+
+# גודל של המסך
 WINDOW_W = 800
 WINDOW_H = 664
 WINDOW_SIZE = (WINDOW_W, WINDOW_H)
-
+# צבע
+BLACK = (0, 0, 0)
+# ריבועים
+square1_y = WINDOW_H/2
+square2_y = WINDOW_H/2
+# הכדור
+circle_x = WINDOW_W/2
+circle_y = WINDOW_H/2
 pygame.init()
 screen = pygame.display.set_mode(WINDOW_SIZE)
 pygame.display.set_caption("pong")
 
-#pygame.draw.line(screen, (20,20,20), (60, 80), (130, 100), 100)
-RED=(255,0,0)
-size = (50, 50)
-points = [(25, 0), (50, 25), (25, 50), (0, 25)]
+hold_player_1 = 0
+hold_player_2 = 0
 
-lines = pygame.Surface(size)
-pygame.draw.lines(lines, RED, False, points)
-
-lines_closed = pygame.Surface(size)
-pygame.draw.lines(lines_closed, RED, True, points)
-
+clock = pygame.time.Clock()
+# הזזה של הריבועים עם המקשים
 play = True
 while play:
     for event in pygame.event.get():
-      if event.type == pygame.QUIT:
-        play = False
-    
-    
-    
+        if event.type == pygame.QUIT:
+            play = False
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_w:
+                hold_player_1 = -1
+            elif event.key == pygame.K_s:
+                hold_player_1 = 1
+            if event.key == pygame.K_UP:
+                hold_player_2 = -1
+            elif event.key == pygame.K_DOWN:
+                hold_player_2 = 1
+        elif event.type == pygame.KEYUP:
+            hold_player_1 = 0
+            hold_player_2 = 0
+
+    square1_y += 20 * hold_player_1
+    square2_y += 20 * hold_player_2
+    screen.fill(BLACK)
+    # מצייר את הקווים,הריבועים והכדור
+    pygame.draw.line(screen, (255, 255, 255), (0, 0), (0, WINDOW_H), 5)
+    pygame.draw.line(screen, (255, 255, 255),
+                     (0, WINDOW_H), (WINDOW_W, WINDOW_H), 5)
+    pygame.draw.line(screen, (255, 255, 255),
+                     (WINDOW_W, WINDOW_H), (WINDOW_W, 0), 5)
+    pygame.draw.line(screen, (255, 255, 255), (WINDOW_W, 0), (0, 0), 5)
+    pygame.draw.line(screen, (255, 255, 255), (400, 0), (400, 664), 5)
+    pygame.draw.line(screen, (232, 31, 63),
+                     (800-15, square2_y+30), (800-15, square2_y-30), 10)
+    pygame.draw.line(screen, (232, 31, 63),
+                     (10, square1_y+30), (10, square1_y-30), 10)
+    pygame.draw.circle(screen, (181, 187, 254), (circle_x, circle_y), 15)
+    pygame.display.flip()
+
+    clock.tick(15)
+
+pygame.display.update()
