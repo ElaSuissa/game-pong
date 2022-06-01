b60b8440719a8d339f0fcc841a4fd14fd159df06
ElaSuissa <97167034+ElaSuissa@users.noreply.github.com>
2022-06-01 14:35:35 +0300

    מוכן אחרי צבע חדש

--- game.py
+++ game.py
@@ -7,13 +7,16 @@ from pickle import TRUE
 import cv2
 import mediapipe as mp
 
-# define a video capture object
+#לאיזה מצלמה המחשב מתחבר
 vid = cv2.VideoCapture(0)
 
-
+#מאתחל את המערכת של זיהוי היידים
 mp_Hands = mp.solutions.hands
+#קולט יידים, מספר מקסימום ורמת ביטחון 
 hands = mp_Hands.Hands(max_num_hands=1,min_detection_confidence=0.7)
+#מצייר את הנקודות על היידים
 mpDraw = mp.solutions.drawing_utils
+
 
 index_finger_y_player1 = 0
 
@@ -23,7 +26,7 @@ WINDOW_H = 664
 WINDOW_SIZE = (WINDOW_W, WINDOW_H)
 
 # צבע
-BLACK = (0, 0, 0)
+WHITE = (255, 255, 225)
 PINK = (253, 57, 72)
 
 # ריבועים
@@ -47,12 +50,12 @@ pygame.display.set_caption("pong")
 #פונקציות שבודקות אם הכדור פגע במלבן של השחקנים 
 def is_circle_hit_player1 (circle_x, circle_y,square1_y):
      return abs (circle_x - 10) < 30 and (circle_y - square1_y ) < 30
-   #  return abs (circle_x - 10) < 30 and abs (circle_y - square1_y ) < 30
+
 
 def is_circle_hit_player2 (circle_y,circle_x, square2_y):
     return (circle_y - square2_y)< 30 and abs ( 800-15 - circle_x)< 30
-   # return abs (circle_y - square2_y)< 30 and abs ( 800-15 - circle_x)< 30
 
+#פונקציה שבודקת אם הכדור יצא מהמשחק והשחקן הפסיד
 def if_lose (circle_x):
     return (circle_x > WINDOW_W) or (circle_x < 0)
 
@@ -81,36 +84,43 @@ while play:
             hold_player_1 = 0
             hold_player_2 = 0  
     square1_y += 20 * hold_player_1   
-    # square2_y += 20 * hold_player_2
+
+
+#קורא את התמונה 
+
     ret, frame = vid.read()
-    max_num_hands=2
+    max_num_hands=1
+    
+    #משנה מ BGR לRGB
     RGB_image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
 
+#מחפש ידיים בתמונה
     results = hands.process(RGB_image)
     multiLandMarks = results.multi_hand_landmarks
+    
+    
+    # אם הוא מוצא ידיים, הוא עובר לך היידים ומצייר אותם 
     if multiLandMarks and len(multiLandMarks)==1:
-            # go over all hands found and draw them on the BGR image
         for handLms in multiLandMarks:
             mpDraw.draw_landmarks(frame, handLms, mp_Hands.HAND_CONNECTIONS)
-        #cv2.imshow('frame', frame)
-
-          # the location of the index finger tip is number 8 in the landmarks
+            
+ #מיקום הנקודות שעל היד
         index_finger8_y_player2 = multiLandMarks[0].landmark[8].y
         index_finger5_y_player2 = multiLandMarks[0].landmark[5].y
-        index_finger8_x_player2 = multiLandMarks[0].landmark[8].x
-        index_finger5_x_player2 = multiLandMarks[0].landmark[5].x  
         index_finger12_y_player2 = multiLandMarks[0].landmark[12].y
         index_finger9_y_player2 = multiLandMarks[0].landmark[9].y
         
-        #print("player1= ",index_finger8_x_player2)
-    
+ 
     
         hold_player_1 = 0
         hold_player_2 = 0
+        
+    #אם האצבע מורמת אז הריבוע עולה למעלה ואם יש שתי אצבעות מורמות אז הואר עוצר במקום
         if (index_finger8_y_player2 < index_finger5_y_player2) and square2_y >= 30 and (index_finger12_y_player2 > index_finger9_y_player2):
             # print ("finger1 =up")
             # hold_player_2 = -1
             square2_y -= 10
+            
         if (index_finger8_y_player2 > index_finger5_y_player2) and square2_y <= WINDOW_H -30 and (index_finger12_y_player2 > index_finger9_y_player2):
         # print ("finger1 =down")
             # hold_player_2 = 1
@@ -168,24 +178,22 @@ while play:
         hold_player_2 = 0
 
         
-    screen.fill(BLACK)
+    screen.fill(WHITE)
 
     
     if if_lose (circle_x):
-        screen.blit(LOSE_IMAGE, (180,200))
+        screen.blit(LOSE_IMAGE, (160,200))
             
 
     # מצייר את הקווים,הריבועים והכדור
-    pygame.draw.line(screen, (255, 255, 255), (0, 0), (0, WINDOW_H), 5)
-    pygame.draw.line(screen, (255, 255, 255),
-                     (0, WINDOW_H), (WINDOW_W, WINDOW_H), 5)
-    pygame.draw.line(screen, (255, 255, 255),
-                     (WINDOW_W, WINDOW_H), (WINDOW_W, 0), 5)
-    pygame.draw.line(screen, (255, 255, 255), (WINDOW_W, 0), (0, 0), 5)
-    pygame.draw.line(screen, (255, 255, 255), (400, 0), (400, 664), 5)
-    pygame.draw.line(screen, (232, 31, 63), (800-15, square2_y+30), (800-15, square2_y-30), 10)
-    pygame.draw.line(screen, (232, 31, 63), (10, square1_y+30), (10, square1_y-30), 10)
-    pygame.draw.circle(screen, (181, 187, 254), (circle_x, circle_y), 15)
+    pygame.draw.line(screen, (0, 0, 0), (0, 0), (0, WINDOW_H), 5)
+    pygame.draw.line(screen, (0, 0, 0), (0, WINDOW_H), (WINDOW_W, WINDOW_H), 5)
+    pygame.draw.line(screen, (0, 0, 0), (WINDOW_W, WINDOW_H), (WINDOW_W, 0), 5)
+    pygame.draw.line(screen, (0, 0, 0), (WINDOW_W, 0), (0, 0), 5)
+    pygame.draw.line(screen, (0, 0, 0), (400, 0), (400, 664), 5)
+    pygame.draw.line(screen, (PINK), (800-15, square2_y+30), (800-15, square2_y-30), 10)
+    pygame.draw.line(screen, (PINK), (10, square1_y+30), (10, square1_y-30), 10)
+    pygame.draw.circle(screen, (PINK), (circle_x, circle_y), 15)
     font = pygame.font.SysFont(None, 50)
     #ניקוד
     img2 = font.render('score:' + str(count2), True, PINK) 
